c3daeaf931f79a554bc3a24b13bcc50bdb587fc4
Veneta Ivanova <i.veneta.v@gmail.com>
2022-10-29 13:56:34 +0300

    main game logic

--- app.js
+++ app.js
@@ -28,29 +28,101 @@ function game() {
 
         parentMainElement.appendChild(resultDivElement)
 
-
         //create main game
         const gameMainElement = document.createElement('div');
 
-        const rockElement = document.createElement('img');
+        const rockElement = document.createElement('div');
+        const rockImg = document.createElement('img');
         rockElement.classList.add('gameEl');
-        rockElement.classList.add('rock');
-        rockElement.setAttribute('src', './img/rock.png')
+        rockElement.setAttribute('id', 'rock')
+        rockImg.setAttribute('src', './img/rock.png');
+        rockElement.appendChild(rockImg);
         gameMainElement.appendChild(rockElement);
 
-        const scissorsElement = document.createElement('img');
+        const scissorsElement = document.createElement('div');
+        const scissorsImg = document.createElement('img');
         scissorsElement.classList.add('gameEl');
-        scissorsElement.classList.add('scissors');
-        scissorsElement.setAttribute('src', './img/scissors.png')
+        scissorsElement.setAttribute('id', 'scissors')
+        scissorsImg.setAttribute('src', './img/scissors.png');
+        scissorsElement.appendChild(scissorsImg);
         gameMainElement.appendChild(scissorsElement);
 
-        const paperElement = document.createElement('img');
+        const paperElement = document.createElement('div');
+        const paperImg = document.createElement('img');
         paperElement.classList.add('gameEl');
-        paperElement.classList.add('paper');
-        paperElement.setAttribute('src', './img/paper.png')
+        paperElement.setAttribute('id', 'paper')
+        paperImg.setAttribute('src', './img/paper.png');
+        paperElement.appendChild(paperImg);
         gameMainElement.appendChild(paperElement);
 
-        parentMainElement.appendChild(gameMainElement)
+        parentMainElement.appendChild(gameMainElement);
+        const allGameEl = Array.from(document.querySelectorAll('.gameEl'));
 
+
+        for (let i = 0; i < allGameEl.length; i++) {
+            const element = allGameEl[i];
+            element.addEventListener('click', playGame);
+        }
+
+        function playGame(еv) {
+            let playerChoice = еv.currentTarget.id;
+            console.log(`Player - ${playerChoice}`);
+            let computerChoise = Math.random();
+            console.log(`Computer - ${computerChoise}`);
+            debugger;
+            if (computerChoise < 0.33) {
+                computerChoise = 'rock';
+            } else if (computerChoise < 0.67) {
+                computerChoise = 'scissors';
+            } else {
+                computerChoise = 'paper';
+            }
+
+            let resultTempGame = checkWinner(playerChoice, computerChoise);
+            if (resultTempGame == 'player') {
+                userResultValue++;
+                userResultDiv.textContent = `${playerName}: ${userResultValue}`;
+            } else if (resultTempGame == 'computer') {
+                computerResultValue++;
+                computerResultDiv.textContent = `Computer: ${computerResultValue}`;
+            } else if (resultTempGame == 'equal') {
+                window.alert('it is a tie!');
+            }
+
+
+            function checkWinner(playerChoice, computerChoise) {
+                if (playerChoice == 'rock') {
+                    if (computerChoise == 'scissors') {
+                        return 'player';
+                    } else if (computerChoise == 'paper') {
+                        return 'computer';
+                    } else {
+                        return 'equal'
+                    }
+                } else if (playerChoice == 'scissors') {
+                    if (computerChoise == 'rock') {
+                        return 'computer';
+                    } else if (computerChoise == 'paper') {
+                        return 'player';
+                    } else {
+                        return 'equal'
+                    }
+                } else if (playerChoice == 'paper') {
+                    if (computerChoise == 'scissors') {
+                        return 'computer';
+                    } else if (computerChoise == 'rock') {
+                        return 'player';
+                    } else {
+                        return 'equal'
+                    }
+                }
+            }
+
+        }
     });
+
+
+
+
+
 }
