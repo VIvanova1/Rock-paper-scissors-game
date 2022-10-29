50d51d43d5570dc479c22a9cb010c2ba73f6ffe6
Veneta Ivanova <i.veneta.v@gmail.com>
2022-10-29 11:18:30 +0300

    create result element

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 window.addEventListener('load', game);
 
 function game() {
-    console.log('work');
+    const startBtnElement = document.getElementById('start');
+    startBtnElement.addEventListener('click', () => {
+        const parentMainElement = document.getElementsByTagName('main')[0];
+        const playerName = document.getElementById('userName').value;
+
+        //remove div for username
+        const playerDiv = document.getElementsByClassName('playerUser');
+        playerDiv[0].remove();
+
+        //create div for result
+        const resultDivElement = document.createElement('div');
+        resultDivElement.classList.add('game');
+
+        const userResultDiv = document.createElement('div');
+        let userResultValue = 0;
+        userResultDiv.textContent = `${playerName}: ${userResultValue}`;
+        resultDivElement.appendChild(userResultDiv);
+
+        const computerResultDiv = document.createElement('div');
+        let computerResultValue = 0;
+        computerResultDiv.textContent = `Computer: ${computerResultValue}`;
+        resultDivElement.appendChild(computerResultDiv);
+
+        parentMainElement.appendChild(resultDivElement)
+
+
+
+    });
 }
