76bde76edcd47c2cbe05339c161fba089e84ec7b
Veneta Ivanova <i.veneta.v@gmail.com>
2022-10-30 17:44:22 +0200

    change event listener

--- app.js
+++ app.js
@@ -1,157 +1,136 @@
-window.addEventListener('load', game);
-
 function game() {
-    const startBtnElement = document.getElementById('start');
-    startBtnElement.addEventListener('click', () => {
-        const parentMainElement = document.getElementsByTagName('main')[0];
-        const playerName = document.getElementById('userName').value;
 
-        //remove div for username
-        const playerDiv = document.getElementsByClassName('playerUser');
-        playerDiv[0].remove();
+    const parentMainElement = document.getElementsByTagName('main')[0];
+    const playerName = document.getElementById('userName').value;
 
-        //create div for result
-        const resultDivElement = document.createElement('div');
-        resultDivElement.classList.add('game');
+    //remove div for username
+    const playerDiv = document.getElementsByClassName('playerUser');
+    playerDiv[0].remove();
 
-        const userResultDiv = document.createElement('div');
-        let userResultValue = 0;
+    //create div for result
+    const resultDivElement = document.createElement('div');
+    resultDivElement.classList.add('game');
+
+    const userResultDiv = document.createElement('div');
+    let userResultValue = 0;
+    userResultDiv.textContent = `${playerName}: ${userResultValue}`;
+    userResultDiv.classList.add('result')
+    resultDivElement.appendChild(userResultDiv);
+
+    const computerResultDiv = document.createElement('div');
+    let computerResultValue = 0;
+    computerResultDiv.textContent = `Computer: ${computerResultValue}`;
+    computerResultDiv.classList.add('result')
+    resultDivElement.appendChild(computerResultDiv);
+
+    parentMainElement.appendChild(resultDivElement)
+
+    //create main game
+    const gameMainElement = document.createElement('div');
+
+    const rockElement = document.createElement('div');
+    const rockImg = document.createElement('img');
+    rockElement.classList.add('gameEl');
+    rockElement.setAttribute('id', 'rock')
+    rockImg.setAttribute('src', './img/rock.png');
+    rockElement.appendChild(rockImg);
+    gameMainElement.appendChild(rockElement);
+
+    const scissorsElement = document.createElement('div');
+    const scissorsImg = document.createElement('img');
+    scissorsElement.classList.add('gameEl');
+    scissorsElement.setAttribute('id', 'scissors')
+    scissorsImg.setAttribute('src', './img/scissors.png');
+    scissorsElement.appendChild(scissorsImg);
+    gameMainElement.appendChild(scissorsElement);
+
+    const paperElement = document.createElement('div');
+    const paperImg = document.createElement('img');
+    paperElement.classList.add('gameEl');
+    paperElement.setAttribute('id', 'paper')
+    paperImg.setAttribute('src', './img/paper.png');
+    paperElement.appendChild(paperImg);
+    gameMainElement.appendChild(paperElement);
+
+    parentMainElement.appendChild(gameMainElement);
+
+    //reset game
+    const resetBtnEl = document.createElement('button');
+    resetBtnEl.textContent = 'Reset Game';
+    resetBtnEl.setAttribute('id', 'resetBtn')
+    resetBtnEl.addEventListener('click', (e) => {
+        userResultValue = 0;
         userResultDiv.textContent = `${playerName}: ${userResultValue}`;
-        userResultDiv.classList.add('result')
-        resultDivElement.appendChild(userResultDiv);
-
-        const computerResultDiv = document.createElement('div');
-        let computerResultValue = 0;
+        computerResultValue = 0;
         computerResultDiv.textContent = `Computer: ${computerResultValue}`;
-        computerResultDiv.classList.add('result')
-        resultDivElement.appendChild(computerResultDiv);
+    });
+    parentMainElement.appendChild(resetBtnEl);
 
-        parentMainElement.appendChild(resultDivElement)
+    const allGameEl = Array.from(document.querySelectorAll('.gameEl'));
 
-        //create main game
-        const gameMainElement = document.createElement('div');
+    for (let i = 0; i < allGameEl.length; i++) {
+        const element = allGameEl[i];
+        element.addEventListener('click', playGame);
+    }
 
-        const rockElement = document.createElement('div');
-        const rockImg = document.createElement('img');
-        rockElement.classList.add('gameEl');
-        rockElement.setAttribute('id', 'rock')
-        rockImg.setAttribute('src', './img/rock.png');
-        rockElement.appendChild(rockImg);
-        gameMainElement.appendChild(rockElement);
 
-        const scissorsElement = document.createElement('div');
-        const scissorsImg = document.createElement('img');
-        scissorsElement.classList.add('gameEl');
-        scissorsElement.setAttribute('id', 'scissors')
-        scissorsImg.setAttribute('src', './img/scissors.png');
-        scissorsElement.appendChild(scissorsImg);
-        gameMainElement.appendChild(scissorsElement);
+    function playGame(еv) {
 
-        const paperElement = document.createElement('div');
-        const paperImg = document.createElement('img');
-        paperElement.classList.add('gameEl');
-        paperElement.setAttribute('id', 'paper')
-        paperImg.setAttribute('src', './img/paper.png');
-        paperElement.appendChild(paperImg);
-        gameMainElement.appendChild(paperElement);
+        //Player Choice
+        let playerChoice = еv.currentTarget.id;
+        const playerImg = document.getElementById(playerChoice);
 
-        parentMainElement.appendChild(gameMainElement);
-
-        //reset game
-        const resetBtnEl = document.createElement('button');
-        resetBtnEl.textContent = 'Reset Game';
-        resetBtnEl.setAttribute('id', 'resetBtn')
-        resetBtnEl.addEventListener('click', (e) => {
-            userResultValue = 0;
-            userResultDiv.textContent = `${playerName}: ${userResultValue}`;
-            computerResultValue = 0;
-            computerResultDiv.textContent = `Computer: ${computerResultValue}`;
-        });
-        parentMainElement.appendChild(resetBtnEl);
-
-        const allGameEl = Array.from(document.querySelectorAll('.gameEl'));
-
-        for (let i = 0; i < allGameEl.length; i++) {
-            const element = allGameEl[i];
-            element.addEventListener('click', playGame);
+        //Computer Choice
+        let computerChoise = Math.random();
+        if ((computerChoise < 0.1) || (computerChoise >= 0.3 && computerChoise < 0.4) || (computerChoise >= 0.6 && computerChoise < 0.7) || (computerChoise >= 0.9)) {
+            computerChoise = 'rock';
+        } else if ((computerChoise < 0.2) || (computerChoise >= 0.4 && computerChoise < 0.5) || (computerChoise >= 0.7 && computerChoise < 0.8)) {
+            computerChoise = 'scissors';
+        } else if ((computerChoise < 0.3) || (computerChoise >= 0.5 && computerChoise < 0.6) || (computerChoise >= 0.8 && computerChoise < 0.9)) {
+            computerChoise = 'paper';
         }
 
+        //Result Game
+        let resultTempGame = checkWinner(playerChoice, computerChoise);
+        if (resultTempGame == 'player') {
+            userResultValue++;
+            userResultDiv.textContent = `${playerName}: ${userResultValue}`;
 
-        function playGame(еv) {
-            debugger;
-            //Player Choice
-            let playerChoice = еv.currentTarget.id;
-            let playerChoiceImg = document.getElementById(playerChoice);
-            let borderPlayer = ''
-            playerChoiceImg.style.border = borderPlayer;
+        } else if (resultTempGame == 'computer') {
+            computerResultValue++;
+            computerResultDiv.textContent = `Computer: ${computerResultValue}`;
+        } else if (resultTempGame == 'equal') {
+            window.alert('it is a tie!');
+        };
 
-            //Computer Choice
-            let computerChoise = Math.random();
-            if (computerChoise < 0.33) {
-                computerChoise = 'rock';
-            } else if (computerChoise < 0.67) {
-                computerChoise = 'scissors';
-            } else {
-                computerChoise = 'paper';
-            }
-            let computerChoiseImg = document.getElementById(computerChoise);
-            let borderComputer = '';
-            computerChoiseImg.style.border = borderComputer;
-
-            //Result Game
-            let resultTempGame = checkWinner(playerChoice, computerChoise);
-            if (resultTempGame == 'player') {
-                userResultValue++;
-                borderPlayer = '5px solid red';
-                playerChoiceImg.style.border = borderPlayer;
-                borderComputer = '1px solid blue';
-                computerChoiseImg.style.border = borderComputer;
-            } else if (resultTempGame == 'computer') {
-                computerResultValue++;
-                computerResultDiv.textContent = `Computer: ${computerResultValue}`;
-                borderPlayer = '1px solid red';
-                playerChoiceImg.style.border = borderPlayer;
-                borderComputer = '5px solid blue';
-                computerChoiseImg.style.border = borderComputer;
-            } else if (resultTempGame == 'equal') {
-                window.alert('it is a tie!');
-            };
-
-            // Check who is the winner
-            function checkWinner(playerChoice, computerChoise) {
-                if (playerChoice == 'rock') {
-                    if (computerChoise == 'scissors') {
-                        return 'player';
-                    } else if (computerChoise == 'paper') {
-                        return 'computer';
-                    } else {
-                        return 'equal'
-                    }
-                } else if (playerChoice == 'scissors') {
-                    if (computerChoise == 'rock') {
-                        return 'computer';
-                    } else if (computerChoise == 'paper') {
-                        return 'player';
-                    } else {
-                        return 'equal'
-                    }
-                } else if (playerChoice == 'paper') {
-                    if (computerChoise == 'scissors') {
-                        return 'computer';
-                    } else if (computerChoise == 'rock') {
-                        return 'player';
-                    } else {
-                        return 'equal'
-                    }
+        // Check who is the winner
+        function checkWinner(playerChoice, computerChoise) {
+            if (playerChoice == 'rock') {
+                if (computerChoise == 'scissors') {
+                    return 'player';
+                } else if (computerChoise == 'paper') {
+                    return 'computer';
+                } else {
+                    return 'equal';
+                }
+            } else if (playerChoice == 'scissors') {
+                if (computerChoise == 'rock') {
+                    return 'computer';
+                } else if (computerChoise == 'paper') {
+                    return 'player';
+                } else {
+                    return 'equal';
+                }
+            } else if (playerChoice == 'paper') {
+                if (computerChoise == 'scissors') {
+                    return 'computer';
+                } else if (computerChoise == 'rock') {
+                    return 'player';
+                } else {
+                    return 'equal';
                 }
             }
-
-
         }
-    });
-
-
-
-
+    }
 
 }
